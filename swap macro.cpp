44806118d421b798f79d54f60c5d36a19f77866a
riyaty4 <50183709+riyaty4@users.noreply.github.com>
2019-10-04 01:52:28 +0530

    Add files via upload

---
+++ swap macro.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+# define swap(x,y,temp) temp=x; x=y; y=temp;
+int main()
+{
+int x1,x2,t;
+cout<<"enter the number ";
+cin>>x1>>x2;
+swap(x1,x2,t);
+cout<<"x1= "<<x1<<"x2="<<x2;
+return 0;
+}
+
